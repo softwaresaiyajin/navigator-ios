11cfebabf2bd4b8178b77482c015edee16db64c2
softwaresaiyajin <geralda.adorza@gmail.com>
2018-09-29 17:55:39 +0800

    first commit

---
+++ Navigator/Navigator.h
@@ -0,0 +1,19 @@
+//
+//  Navigator.h
+//  Navigator
+//
+//  Created by Gerald Adorza on 29/9/2018.
+//  Copyright © 2018 Gerald Adorza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Navigator.
+FOUNDATION_EXPORT double NavigatorVersionNumber;
+
+//! Project version string for Navigator.
+FOUNDATION_EXPORT const unsigned char NavigatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Navigator/PublicHeader.h>
+
+
